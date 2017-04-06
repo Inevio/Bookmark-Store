0561f8032ac8fcb515eecf5e290ab7e45bae7af0
Carlos Navas <carlosnavas1991@gmail.com>
2017-04-06 09:58:13 +0200

    Using prototypes

--- static/script.js
+++ static/script.js
@@ -1,109 +1,150 @@
 
-    var win = $( this );
+var win = $( this );
+var bookmarkFinder = $( '.bookmark-finder' );
+var appPrototype = $('.bookmark-card.wz-prototype');
 
-    $( '.bookmark-finder input' ).focus( function(){
-        $( '.bookmark-finder' ).addClass( 'active' );
-    });
+//EVENTOS
+win.on( 'focus', '.bookmark-finder input', function(){
+  bookmarkFinder.addClass( 'active' );
+})
 
-    $( '.bookmark-finder input' ).blur( function(){
-        $( '.bookmark-finder' ).removeClass( 'active' );
-    });
+.on( 'blur', '.bookmark-finder input', function(){
+  bookmarkFinder.removeClass( 'active' );
+})
 
-    $( '.bookmark-finder' ).mousedown( function(){
-        $( '.bookmark-finder' ).addClass( 'active' );
-    });
+.on( 'mousedown', '.bookmark-finder', function(){
+  bookmarkFinder.addClass( 'active' );
+})
 
-    $( '.bookmark-finder' ).click( function(){
-        $( '.bookmark-finder input' ).focus();
-    });
+.on( 'click', '.bookmark-finder', function(){
+  $( '.bookmark-finder input' ).focus();
+});
 
-    /*
 
-    $( '.content-subject-text input' ).focus();
+//FUNCIONES
+var addApp = function( id ){}
 
-    win.on( 'click', '.content-button', function(){
+var loadAppList = function(){
 
-        if( $( '.content-subject-text input' ).val() && $( '.content-description-text' ).val() ){
+  var appList = [];
 
-            api.feedback( $( '.content-subject-text input' ).val(), $( '.content-description-text' ).val(), function( error ){
+  for( var i = 0; i < 50; i++ ){
 
-                if( error ){
-                    console.log( error );
-                }
+    var app = appPrototype.clone();
 
-            });
+    app
+    .removeClass( 'wz-prototype' )
+    .addClass( 'appDom' );
+    //.addClass( 'appDom-' + app.id );
 
-            api.banner()
-                .setTitle( lang.bannerTitle )
-                .setText( lang.bannerDescription )
-                .setIcon( 'https://static.inevio.com/app/9/icon.png' )
-                .render();
+    app.find( '.bookmark-name' ).text( 'Files' );
+    app.find( '.bookmark-button span' ).text( lang.addApp );
 
-            api.view.remove();
+    appList.push( app );
 
-        }else{
+  }
 
-            if( !$( '.content-subject-text input' ).val() && !$( '.content-description-text' ).val() ){
-                alert( lang.errorTotal );
-            }else if( !$( '.content-subject-text input' ).val() ){
-                alert( lang.errorSubject );
-            }else{
-                alert( lang.errorDescription );
+  showListDom( appList );
+
+}
+
+var orderAppList = function( list ){}
+
+var removeApp = function( id ){}
+
+var showListDom = function( list ){
+  $( '.ui-elements' ).append( list );
+}
+
+loadAppList();
+
+/*
+
+$( '.content-subject-text input' ).focus();
+
+win.on( 'click', '.content-button', function(){
+
+    if( $( '.content-subject-text input' ).val() && $( '.content-description-text' ).val() ){
+
+        api.feedback( $( '.content-subject-text input' ).val(), $( '.content-description-text' ).val(), function( error ){
+
+            if( error ){
+                console.log( error );
             }
-
-        }
-
-    });
-
-    win.css({'border-radius'    : '6px',
-             'background-color' : '#2c3238'
-    });
-
-    $( '.app-title' ).text( lang.appTitle );
-    $( '.content-subject-text input' ).attr( 'placeholder' , lang.typehere);
-    $( '.content-description-text' ).attr( 'placeholder' , lang.typehere);
-    $( '.contact-info' ).text( lang.contactInfo );
-    $( '.contact-subject' ).text( lang.contactSubject );
-    $( '.contact-description' ).text( lang.contactDescription );
-    $( '.content-button span' ).text( lang.contactSend );
-
-    searchWorldCard.on( 'input' , function(){
-
-      searchWorldQuery = searchWorldQuery + 1;
-      var searchWorldQueryCopy = searchWorldQuery;
-      filterWorldCards( $( this ).val() , searchWorldQueryCopy );
-
-    });
-
-    var filterWorldCards = function( filter , searchWorldQueryCopy ){
-
-      var worldCards = $( '.world-card' );
-
-      if ( filter === '' ) {
-
-        worldCards.show();
-        return;
-
-      }
-
-      wz.cosmos.list( filter , null , {from:0 , to:1000} , function( e , worlds ){
-
-        // Query desfasada
-        if ( searchWorldQuery != searchWorldQueryCopy ) {
-          return;
-        }
-
-        worldCards.hide();
-
-        $.each( worlds , function( i , world ){
-
-          $( '.world-card-' + world.id ).show();
 
         });
 
+        api.banner()
+            .setTitle( lang.bannerTitle )
+            .setText( lang.bannerDescription )
+            .setIcon( 'https://static.inevio.com/app/9/icon.png' )
+            .render();
 
-      });
+        api.view.remove();
+
+    }else{
+
+        if( !$( '.content-subject-text input' ).val() && !$( '.content-description-text' ).val() ){
+            alert( lang.errorTotal );
+        }else if( !$( '.content-subject-text input' ).val() ){
+            alert( lang.errorSubject );
+        }else{
+            alert( lang.errorDescription );
+        }
 
     }
 
-    */
+});
+
+win.css({'border-radius'    : '6px',
+         'background-color' : '#2c3238'
+});
+
+$( '.app-title' ).text( lang.appTitle );
+$( '.content-subject-text input' ).attr( 'placeholder' , lang.typehere);
+$( '.content-description-text' ).attr( 'placeholder' , lang.typehere);
+$( '.contact-info' ).text( lang.contactInfo );
+$( '.contact-subject' ).text( lang.contactSubject );
+$( '.contact-description' ).text( lang.contactDescription );
+$( '.content-button span' ).text( lang.contactSend );
+
+searchWorldCard.on( 'input' , function(){
+
+  searchWorldQuery = searchWorldQuery + 1;
+  var searchWorldQueryCopy = searchWorldQuery;
+  filterWorldCards( $( this ).val() , searchWorldQueryCopy );
+
+});
+
+var filterWorldCards = function( filter , searchWorldQueryCopy ){
+
+  var worldCards = $( '.world-card' );
+
+  if ( filter === '' ) {
+
+    worldCards.show();
+    return;
+
+  }
+
+  wz.cosmos.list( filter , null , {from:0 , to:1000} , function( e , worlds ){
+
+    // Query desfasada
+    if ( searchWorldQuery != searchWorldQueryCopy ) {
+      return;
+    }
+
+    worldCards.hide();
+
+    $.each( worlds , function( i , world ){
+
+      $( '.world-card-' + world.id ).show();
+
+    });
+
+
+  });
+
+}
+
+*/
